744480891e61b0ae832f1e9154501b16e97ead1e
jlennomatias <john.x12@hotmail.com>
2022-09-07 14:20:33 -0300

    Curso2_python_web

--- primeiro_projeto/settings.py
+++ primeiro_projeto/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'receitas',
+    'pessoas',
 ]
 
 MIDDLEWARE = [
@@ -123,6 +124,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'primeiro_projeto/static')]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- receitas/models.py
+++ receitas/models.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 from django.db import models
+from pessoas.models import Pessoa
 
 # Create your models here.
 class Receita(models.Model):
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
     nome_receita = models.CharField(max_length=200)
     ingredientes = models.TextField()
     modo_preparo = models.TextField()
@@ -10,3 +12,7 @@ class Receita(models.Model):
     rendimento = models.CharField(max_length=100)
     categoria = models.CharField(max_length=100)
     data_receita = models.DateTimeField(default=datetime.now, blank=True)
+    publicada = models.BooleanField(default=False)
+    foto_receita = models.ImageField(upload_to='fotos/%d/%Y', blank=True)
+    def __str__(self):
+        return self.nome_receita
